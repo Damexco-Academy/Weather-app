92499548be8d855722f6725f85790a7c52096b1c
Omodamola Oladeji <darmhoo@hotmail.com>
2024-02-12 12:29:08 +0100

    done

--- js/main.js
+++ js/main.js
@@ -7,33 +7,55 @@ let newLocation = document.getElementById('location');
 let wind = document.getElementById('wind');
 
 
-searchButton.addEventListener('click', ()=> {
-    let weatherResult = getWeatherInfo(city.value);
+searchButton.addEventListener('click', async () => {
+    let loc = await getCityLoc(city.value);
+    console.log(loc);
+    let weatherResult = getWeatherInfo(loc[0].lat, loc[0].lon);
 })
 
-async function getWeatherInfo(cityName){
-    const url = 'https://the-weather-api.p.rapidapi.com/api/weather/' + cityName;
+async function getWeatherInfo(latt, long) {
+    const url = `https://weatherapi-com.p.rapidapi.com/current.json?q=${long}%2C${latt}`;
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'ef23e922edmsh11445b34e6ca89fp1764bfjsn198673c664e6',
-		'X-RapidAPI-Host': 'the-weather-api.p.rapidapi.com'
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
 	}
 };
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.json();
-    console.log(result);
-    tempText.innerHTML = result.data.temp;
-    sumText.innerHTML = result.data.aqi_remark;
-    image.src = result.data.bg_image;
-    newLocation.innerHTML = result.data.city;
-    wind.innerHTML = result.data.wind;
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        tempText.innerHTML = result.data.temp;
+        sumText.innerHTML = result.data.aqi_remark;
+        image.src = result.data.bg_image;
+        newLocation.innerHTML = result.data.city;
+        wind.innerHTML = result.data.wind;
 
-    
-	
-} catch (error) {
-	console.error(error);
+
+
+    } catch (error) {
+        console.error(error);
+    }
 }
+
+async function getCityLoc() {
+    const url = 'https://weatherapi-com.p.rapidapi.com/search.json?q=lagos';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'ef23e922edmsh11445b34e6ca89fp1764bfjsn198673c664e6',
+            'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+        console.error(error);
+        return error;
+    }
 }
